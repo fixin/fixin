216bb6355e1f1e959f18faa0a20b103f26253f73
AttilaJenei <work@attilajenei.com>
2016-05-08 17:31:24 +0200

    PSR-4 Compatible SimpleLoader

---
+++ classes/Fixin/Loader/SimpleLoader.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Fixin\Loader;
+
+class SimpleLoader {
+
+    /**
+     * Registered paths
+     *
+     * @var array
+     */
+    protected $paths = [];
+
+    /**
+     * @param array $namespaces
+     */
+    public function __construct(array $namespaces = []) {
+        $this->addNamespaces($namespaces);
+    }
+
+    /**
+     * Adds a base path for a prefix
+     *
+     * @param string $prefix
+     * @param string|array $path
+     * @return \Fixin\Loader\SimpleLoader
+     */
+    public function addNamespace($prefix, $path) {
+        // Prepare prefix
+        $prefix = trim($prefix, '\\');
+        if (DIRECTORY_SEPARATOR !== '\\') {
+            $prefix = strtr($prefix, '\\', DIRECTORY_SEPARATOR);
+        }
+
+        // Add normalized path(s)
+        foreach (is_array($path) ? $path : [$path] as $item) {
+            $this->paths[$prefix][] = rtrim($item, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Adds multiple namespaces
+     *
+     * @param array $namespaces
+     * @return \Fixin\Loader\SimpleLoader
+     */
+    public function addNamespaces(array $namespaces) {
+        foreach ($namespaces as $prefix => $path) {
+            $this->addNamespace($prefix, $path);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Load class source file if exists
+     *
+     * @param string $class
+     */
+    public function autoload($class) {
+        // Swap '\'
+        if (DIRECTORY_SEPARATOR !== '\\') {
+            $class = strtr($class, '\\', DIRECTORY_SEPARATOR);
+        }
+
+        $length = 0;
+
+        while (($length = strpos($class, DIRECTORY_SEPARATOR, $length + 1)) !== false) {
+            $prefix = substr($class, 0, $length);
+
+            // Prefix found
+            if (isset($this->paths[$prefix])) {
+                $relativeName = substr($class, $length + 1) . '.php';
+
+                // Walking through the possible paths
+                foreach ($this->paths[$prefix] as $path) {
+                    $filename = $path . $relativeName;
+
+                    // Source file found
+                    if (file_exists($filename)) {
+                        require $filename;
+
+                        return;
+                    }
+                }
+
+                return;
+            }
+        }
+    }
+
+    /**
+     * Register to autoloader stack
+     *
+     * @return \Fixin\Loader\SimpleLoader
+     */
+    public function register() {
+        spl_autoload_register(array($this, 'autoload'));
+
+        return $this;
+    }
+}
