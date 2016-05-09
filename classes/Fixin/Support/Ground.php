82b9f48097eb76921e78a90d1d7d70f48594a3f9
AttilaJenei <work@attilajenei.com>
2016-05-09 16:40:17 +0200

    Ground

---
+++ classes/Fixin/Support/Ground.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fixin\Support;
+
+class Ground {
+
+    use DoNotCreateTrait;
+
+    /**
+     * @var bool
+     */
+    protected static $isConsole;
+
+    /**
+     * Checks if running in CLI mode
+     *
+     * @return boolean
+     */
+    static function isConsole() {
+        return static::$isConsole ?? (static::$isConsole = PHP_SAPI == 'cli');
+    }
+}
