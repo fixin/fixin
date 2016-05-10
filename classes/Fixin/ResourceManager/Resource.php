6bbafa6878a58bba7e6bd7557d64b0554dc11673
AttilaJenei <work@attilajenei.com>
2016-05-10 12:36:53 +0200

    Resource

---
+++ classes/Fixin/ResourceManager/Resource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Fixin\ResourceManager;
+
+use Fixin\Support\ContainerInterface;
+
+abstract class Resource implements ResourceInterface {
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @param ContainerInterface $container
+     * @param array $options
+     */
+    public function __construct(ContainerInterface $container, array $options = []) {
+        $this->container = $container;
+    }
+}
