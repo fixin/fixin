8e32805c4500f990817e3f33ff5e1abfd5d10c42
AttilaJenei <www@attilajenei.com>
2016-05-12 20:46:47 +0200

    Removed from global

--- cheats/web.php
+++ cheats/web.php
@@ -1,14 +1,18 @@
 <?php
-$fixinPath = dirname(__DIR__);
+function init() {
+    $fixinPath = dirname(__DIR__);
 
-// Autoloader
-$classesPath = "{$fixinPath}/classes";
-require "{$classesPath}/Fixin/Base/Autoloader/SimpleAutoloader.php";
-$autoloader = new \Fixin\Base\Autoloader\SimpleAutoloader(['Fixin' => "{$classesPath}/Fixin"]);
-$autoloader->register();
+    // Autoloader
+    $classesPath = "{$fixinPath}/classes";
+    require "{$classesPath}/Fixin/Base/Autoloader/SimpleAutoloader.php";
+    $autoloader = new \Fixin\Base\Autoloader\SimpleAutoloader(['Fixin' => "{$classesPath}/Fixin"]);
+    $autoloader->register();
 
-// Config
-$config = require "{$fixinPath}/config/web.php";
+    // Config
+    $config = require "{$fixinPath}/config/web.php";
 
-// Application
-return new \Fixin\Application\Application($config);
+    // Application
+    return new \Fixin\Application\Application($config);
+}
+
+return init();
