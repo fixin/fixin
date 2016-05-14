e9280ee8fadfc483a4b0142d18cc5fea0c974518
AttilaJenei <www@attilajenei.com>
2016-05-15 00:32:34 +0200

    Refactor to getPath()

--- classes/Fixin/Base/Uri/Factory/EnvironmentUriFactory.php
+++ classes/Fixin/Base/Uri/Factory/EnvironmentUriFactory.php
@@ -17,40 +17,32 @@ class EnvironmentUriFactory implements FactoryInterface {
      * {@inheritDoc}
      * @see \Fixin\ResourceManager\Factory\FactoryInterface::__invoke()
      */
-    public function __invoke(ContainerInterface $container, string $name = null) {
+    public function __invoke(ContainerInterface $container, string $name) {
         $uri = new Uri();
         $uri->setScheme(($https = $_SERVER['HTTPS'] ?? false) && $https !== 'off' ? 'https' : 'http')
         ->setHost($_SERVER['HTTP_HOST'])
         ->setPort($_SERVER['SERVER_PORT'])
-        ->setPath(($index = strpos($path = $this->getUriString(), '?')) ? substr($path, 0, $index) : $path)
+        ->setPath($this->getPath())
         ->setQuery($_SERVER['QUERY_STRING']);
 
         return $uri;
     }
 
     /**
-     * Get URI string
+     * Get path
      *
      * @throws InvalidConfigException
      * @return string
      */
-    protected function getUriString(): string {
-        if ($requestUri = $_SERVER['HTTP_X_REWRITE_URL'] ?? false) {
-            return $requestUri;
+    protected function getPath(): string {
+        $uri = $_SERVER['HTTP_X_REWRITE_URL'] ?? $_SERVER['REQUEST_URI'] ?? $_SERVER['ORIG_PATH_INFO'] ?? function() {
+            throw new InvalidConfigException('Can\'t determine the request URI');
+        };
+
+        if (null === $index = strpos($path = $this->getUriString(), '?')) {
+            return $uri;
         }
 
-        if ($requestUri = $_SERVER['REQUEST_URI']) {
-            if ($requestUri[0] === '/') {
-                return $requestUri;
-            }
-
-            return preg_replace('/^(http|https):\/\/[^\/]+/i', '', $requestUri);
-        }
-
-        if ($requestUri = $_SERVER['ORIG_PATH_INFO'] ?? false) {
-            return $requestUri;
-        }
-
-        throw new InvalidConfigException('Can\'t determine the request URI');
+        return substr($uri, 0, $index);
     }
 }
