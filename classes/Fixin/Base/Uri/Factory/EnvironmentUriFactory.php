1f08aa7715fee34cd2254ce8988074c180d631ec
AttilaJenei <www@attilajenei.com>
2016-05-14 16:18:50 +0200

    EnvironmentUriFactory

---
+++ classes/Fixin/Base/Uri/Factory/EnvironmentUriFactory.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Fixin Framework
+ *
+ * @copyright  Copyright (c) 2016 Attila Jenei
+ */
+
+namespace Fixin\Base\Uri\Factory;
+
+use Fixin\Base\Uri\Uri;
+use Fixin\ResourceManager\Factory\FactoryInterface;
+use Fixin\Support\ContainerInterface;
+
+class EnvironmentUriFactory implements FactoryInterface {
+
+    /**
+     * {@inheritDoc}
+     * @see \Fixin\ResourceManager\Factory\FactoryInterface::__invoke()
+     */
+    public function __invoke(ContainerInterface $container, string $name = null) {
+        $uri = new Uri();
+        $uri->setScheme(($https = $_SERVER['HTTPS'] ?? false) && $https !== 'off' ? 'https' : 'http')
+        ->setHost($_SERVER['HTTP_HOST'])
+        ->setPort($_SERVER['SERVER_PORT'])
+        ->setPath(($index = strpos($path = $this->getUriString(), '?')) ? substr($path, 0, $index) : $path)
+        ->setQuery($_SERVER['QUERY_STRING']);
+
+        return $uri;
+    }
+
+    /**
+     * Get URI string
+     *
+     * @throws InvalidConfigException
+     * @return string
+     */
+    protected function getUriString(): string {
+        if ($requestUri = $_SERVER['HTTP_X_REWRITE_URL'] ?? false) {
+            return $requestUri;
+        }
+
+        if ($requestUri = $_SERVER['REQUEST_URI']) {
+            if ($requestUri[0] !== '/') {
+                $requestUri = preg_replace('/^(http|https):\/\/[^\/]+/i', '', $requestUri);
+            }
+
+            return $requestUri;
+        }
+
+        if ($requestUri = $_SERVER['ORIG_PATH_INFO'] ?? false) {
+            return $requestUri;
+        }
+
+        throw new InvalidConfigException('Can\'t determine the request URI');
+    }
+}
