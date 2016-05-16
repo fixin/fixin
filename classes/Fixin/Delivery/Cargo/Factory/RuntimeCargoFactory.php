e9b03fced39916b4da12056875b5069e43674bd6
AttilaJenei <www@attilajenei.com>
2016-05-17 01:30:38 +0200

    RuntimeCargoFactory first

---
+++ classes/Fixin/Delivery/Cargo/Factory/RuntimeCargoFactory.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Fixin Framework
+ *
+ * @copyright  Copyright (c) 2016 Attila Jenei
+ */
+
+namespace Fixin\Delivery\Cargo\Factory;
+
+use Fixin\ResourceManager\Factory\FactoryInterface;
+use Fixin\ResourceManager\ResourceManagerInterface;
+use Fixin\Support\Ground;
+
+class RuntimeCargoFactory implements FactoryInterface {
+
+    /**
+     * {@inheritDoc}
+     * @see \Fixin\ResourceManager\Factory\FactoryInterface::__invoke()
+     */
+    public function __invoke(ResourceManagerInterface $container, string $name = null) {
+        $factory = new HttpCargoFactory();
+
+        return $factory($container, $name);
+    }
+}
