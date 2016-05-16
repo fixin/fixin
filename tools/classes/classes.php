c9c0610cec4eca4789411ea25dc979b04c36a1ea
AttilaJenei <www@attilajenei.com>
2016-05-16 20:58:31 +0200

    Classes refactor

--- tools/classes/classes.php
+++ tools/classes/classes.php
@@ -9,75 +9,7 @@ $topDir = dirname(__DIR__, 2);
 $application = include "$topDir/cheats/web.php";
 
 // Functions
-function classShortName($name): string {
-    return substr(strrchr($name, '\\'), 1);
-}
-
-function classLink($name): string {
-    $name = ltrim($name, '\\');
-    return strncmp($name, 'Fixin\\', 6)
-    ? htmlspecialchars($name)
-    : '<a href="#' . htmlspecialchars($name) . '">' . htmlspecialchars(classShortName($name)) . '</a>';
-}
-
-function commentText($reflection) {
-    if (preg_match_all('/^\s*\*\s*([^@\s*].+)$/m', $reflection->getDocComment(), $matches)) {
-        if ($matches[1][0] === '{@inheritDoc}' && ($parent = $reflection->getPrototype())) {
-            return '{@inheritDoc} ' . commentText($parent);
-        }
-    }
-
-    return nl2br(htmlspecialchars(implode("\n", $matches[1])));
-}
-
-function commentParameters($reflection) {
-    $parameters = [];
-
-    preg_match_all('/^\s*\*\s*@param\s+([^\s]+)\s+\$([^\s]+)$/m', $reflection->getDocComment(), $matches);
-
-    foreach ($matches[1] as $index => $type) {
-        $parameters[$matches[2][$index]] = implode('|', array_map('classLink', (explode('|', $type))));
-    }
-
-    return $parameters;
-}
-
-function commentVar($reflection) {
-    if (preg_match_all('(@var\s+([^\s]+))', $reflection->getDocComment(), $matches)) {
-        return implode('|', array_map('classLink', (explode('|', $matches[1][0]))));
-    }
-
-    return '';
-}
-
-function reflectionLink($reflection): string {
-    $name = $reflection->getName();
-
-    return strncmp($name, 'Fixin\\', 6)
-    ? '\\' . htmlspecialchars($name)
-    : '<a href="#' . htmlspecialchars($name) . '">' . htmlspecialchars($reflection->getShortName()) . '</a>';
-}
-
-function orderedReflectionList(array $reflections): array {
-    $list = [];
-
-    foreach ($reflections as $reflection) {
-        $list[$reflection->getName()] = $reflection;
-    }
-
-    ksort($list);
-
-    return $list;
-}
-
-// Row even and odd styles
-$rowEvenState = 0;
-function evenStyle() {
-    global $rowEvenState;
-
-    $rowEvenState = 1 - $rowEvenState;
-    return $rowEvenState === 1 ? 'Even' : 'Odd';
-}
+include 'functions.php';
 
 // Include all PHP files under classes/
 $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator("$topDir/classes"));
@@ -303,7 +235,6 @@ td.Tab {
     				<?php if ($constants = $reflection->getConstants()): ?>
 						<?php
                             ksort($constants);
-                            $rowEvenState = 0;
                         ?>
 						<?php foreach ($constants as $key => $value): ?>
 							<tr class="Element Const <?= evenStyle() ?>">
