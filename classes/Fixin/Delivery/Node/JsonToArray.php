4b426956894f238cf2235be0c47ad96baaca9fc0
AttilaJenei <www@attilajenei.com>
2016-05-17 18:39:07 +0200

    JSON to array

---
+++ classes/Fixin/Delivery/Node/JsonToArray.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Fixin Framework
+ *
+ * @copyright  Copyright (c) 2016 Attila Jenei
+ */
+
+namespace Fixin\Delivery\Node;
+
+use Fixin\Delivery\Cargo\CargoInterface;
+use Fixin\Delivery\Cargo\HttpCargo;
+
+class JsonToArray implements NodeInterface {
+
+    const JSON_TYPES = ['application/json', 'application/jsonml+json'];
+
+    /**
+     * {@inheritDoc}
+     * @see \Fixin\Delivery\Node\NodeInterface::handle()
+     */
+    public function handle(CargoInterface $cargo) {
+        if (in_array($cargo->getContentType(), static::JSON_TYPES)) {
+            $cargo->setContent(json_decode($cargo->getContent(), true, 512, JSON_BIGINT_AS_STRING));
+        }
+
+        return $cargo;
+    }
+}
