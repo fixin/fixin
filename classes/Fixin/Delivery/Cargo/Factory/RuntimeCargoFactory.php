22879f896ea95938441f9a2cd699b6dc505a1e5a
AttilaJenei <www@attilajenei.com>
2016-05-17 21:55:56 +0200

    ConsoleCargoFactory

--- classes/Fixin/Delivery/Cargo/Factory/RuntimeCargoFactory.php
+++ classes/Fixin/Delivery/Cargo/Factory/RuntimeCargoFactory.php
@@ -9,6 +9,7 @@ namespace Fixin\Delivery\Cargo\Factory;
 
 use Fixin\ResourceManager\Factory\FactoryInterface;
 use Fixin\ResourceManager\ResourceManagerInterface;
+use Fixin\Support\Ground;
 
 class RuntimeCargoFactory implements FactoryInterface {
 
@@ -17,6 +18,8 @@ class RuntimeCargoFactory implements FactoryInterface {
      * @see \Fixin\ResourceManager\Factory\FactoryInterface::__invoke()
      */
     public function __invoke(ResourceManagerInterface $container, array $options = null, string $name = null) {
-        return $container->clonePrototype('Delivery\Cargo\Factory\HttpCargoFactory');
+        $factory = 'Delivery\Cargo\Factory\\' . (Ground::isConsole() ? 'ConsoleCargoFactory' : 'HttpCargoFactory');
+
+        return $container->clonePrototype($factory);
     }
 }
