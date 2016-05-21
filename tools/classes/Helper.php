4a97f8d20088804ae6d572db4a8a42b2c8c72b3b
AttilaJenei <www@attilajenei.com>
2016-05-21 17:57:33 +0200

    Parameters

--- tools/classes/Helper.php
+++ tools/classes/Helper.php
@@ -8,11 +8,24 @@
  */
 namespace Classes;
 
+use \Reflector;
+
 class Helper {
 
+    /**
+     * @var array
+     */
     public $namespaces = [];
+
+    /**
+     * @var array
+     */
     protected $shortNameResolve = [];
 
+    /**
+     * @param string $topDir
+     * @param array $rootNamespaces
+     */
     public function __construct(string $topDir, array $rootNamespaces = ['Fixin']) {
         // Include all PHP files under classes/
         $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator("$topDir/classes"));
@@ -25,6 +38,111 @@ class Helper {
         $this->processElements($rootNamespaces);
     }
 
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function classShortName(string $name): string {
+        return substr(strrchr($name, '\\'), 1);
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function classLink(string $name): string {
+        $name = ltrim($name, '\\');
+
+        $name = $this->shortNameResolve[$name] ?? $name;
+
+        return strncmp($name, 'Fixin\\', 6)
+        ? htmlspecialchars($name)
+        : '<a href="#' . htmlspecialchars($name) . '">' . htmlspecialchars($this->classShortName($name)) . '</a>';
+    }
+
+    /**
+     * @param Reflector $reflection
+     * @return string
+     */
+    public function commentText(Reflector $reflection) {
+        if (preg_match_all('/^\s*\*\s*([^@\s*].+)$/m', $reflection->getDocComment(), $matches)) {
+            if ($matches[1][0] === '{@inheritDoc}' && ($parent = $reflection->getPrototype())) {
+                return '<span class="Inherited">' . $this->commentText($parent) . '</span>';
+            }
+        }
+
+        return nl2br(htmlspecialchars(implode("\n", $matches[1])));
+    }
+
+    /**
+     * @param Reflector $reflection
+     * @return string[]
+     */
+    public function commentParameters(Reflector $reflection) {
+        $parameters = [];
+
+        preg_match_all('/^\s*\*\s*@param\s+([^\s]+)\s+\$([^\s]+)$/m', $reflection->getDocComment(), $matches);
+
+        foreach ($matches[1] as $index => $type) {
+            $parameters[$matches[2][$index]] = '<span class="FromComment">' . implode('|', array_map([$this, 'classLink'], (explode('|', $type)))) . '</span>';
+        }
+
+        return $parameters;
+    }
+
+    /**
+     * @param Reflector $reflection
+     * @return string
+     */
+    public function commentVar(Reflector $reflection) {
+        if (preg_match_all('(@var\s+([^\s]+))', $reflection->getDocComment(), $matches)) {
+            return '<span class="FromComment">' . implode('|', array_map([$this, 'classLink'], (explode('|', $matches[1][0])))) . '</span>';
+        }
+
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    public function evenStyle() {
+        static $rowEvenState = 0;
+
+        $rowEvenState = 1 - $rowEvenState;
+        return $rowEvenState === 1 ? 'Even' : 'Odd';
+    }
+
+    /**
+     * @param array $reflections
+     * @return array
+     */
+    public function orderedReflectionList(array $reflections): array {
+        $list = [];
+
+        foreach ($reflections as $reflection) {
+            $list[$reflection->getName()] = $reflection;
+        }
+
+        ksort($list);
+
+        return $list;
+    }
+
+    /**
+     * @param Reflector $reflection
+     * @return string
+     */
+    public function reflectionLink(Reflector $reflection): string {
+        $name = $reflection->getName();
+
+        return strncmp($name, 'Fixin\\', 6)
+        ? '\\' . htmlspecialchars($name)
+        : '<a href="#' . htmlspecialchars($name) . '">' . htmlspecialchars($reflection->getShortName()) . '</a>';
+    }
+
+    /**
+     * @param array $rootNamespaces
+     */
     protected function processElements(array $rootNamespaces) {
         foreach (array_merge(get_declared_classes(), get_declared_interfaces(), get_declared_traits()) as $name) {
             if (($root = strstr($name, '\\', true)) && array_search($root, $rootNamespaces) !== false) {
@@ -37,76 +155,5 @@ class Helper {
         };
 
         ksort($this->namespaces);
-    }
-
-    public function classShortName($name): string {
-        return substr(strrchr($name, '\\'), 1);
-    }
-
-    public function classLink($name): string {
-        $name = ltrim($name, '\\');
-
-        $name = $this->shortNameResolve[$name] ?? $name;
-
-        return strncmp($name, 'Fixin\\', 6)
-        ? htmlspecialchars($name)
-        : '<a href="#' . htmlspecialchars($name) . '">' . htmlspecialchars($this->classShortName($name)) . '</a>';
-    }
-
-    public function commentText($reflection) {
-        if (preg_match_all('/^\s*\*\s*([^@\s*].+)$/m', $reflection->getDocComment(), $matches)) {
-            if ($matches[1][0] === '{@inheritDoc}' && ($parent = $reflection->getPrototype())) {
-                return '<span class="Inherited">' . $this->commentText($parent) . '</span>';
-            }
-        }
-
-        return nl2br(htmlspecialchars(implode("\n", $matches[1])));
-    }
-
-    public function commentParameters($reflection) {
-        $parameters = [];
-
-        preg_match_all('/^\s*\*\s*@param\s+([^\s]+)\s+\$([^\s]+)$/m', $reflection->getDocComment(), $matches);
-
-        foreach ($matches[1] as $index => $type) {
-            $parameters[$matches[2][$index]] = '<span class="FromComment">' . implode('|', array_map([$this, 'classLink'], (explode('|', $type)))) . '</span>';
-        }
-
-        return $parameters;
-    }
-
-    public function commentVar($reflection) {
-        if (preg_match_all('(@var\s+([^\s]+))', $reflection->getDocComment(), $matches)) {
-            return '<span class="FromComment">' . implode('|', array_map([$this, 'classLink'], (explode('|', $matches[1][0])))) . '</span>';
-        }
-
-        return '';
-    }
-
-    public function evenStyle() {
-        static $rowEvenState = 0;
-
-        $rowEvenState = 1 - $rowEvenState;
-        return $rowEvenState === 1 ? 'Even' : 'Odd';
-    }
-
-    public function reflectionLink($reflection): string {
-        $name = $reflection->getName();
-
-        return strncmp($name, 'Fixin\\', 6)
-        ? '\\' . htmlspecialchars($name)
-        : '<a href="#' . htmlspecialchars($name) . '">' . htmlspecialchars($reflection->getShortName()) . '</a>';
-    }
-
-    public function orderedReflectionList(array $reflections): array {
-        $list = [];
-
-        foreach ($reflections as $reflection) {
-            $list[$reflection->getName()] = $reflection;
-        }
-
-        ksort($list);
-
-        return $list;
     }
 }
