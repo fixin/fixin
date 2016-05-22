4977fa14e107c0ded423abcdbe078e64fe2fa36a
AttilaJenei <www@attilajenei.com>
2016-05-23 00:54:18 +0200

    Engine

---
+++ classes/Fixin/View/Engine/Engine.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Fixin Framework
+ *
+ * @copyright  Copyright (c) 2016 Attila Jenei
+ */
+
+namespace Fixin\View\Engine;
+
+use Fixin\ResourceManager\Resource;
+use Fixin\View\ViewInterface;
+
+class Engine extends Resource implements EngineInterface {
+
+    const EXCEPTION_NAME_COLLISION = "Child-variable name collision: '%s'";
+
+    /**
+     * {@inheritDoc}
+     * @see \Fixin\View\Engine\EngineInterface::render()
+     */
+    public function render(ViewInterface $view) {
+        return $this->renderChain($view);
+    }
+
+    /**
+     * Render chain method
+     *
+     * @param ViewInterface $view
+     * @throws KeyCollisionException
+     * @return mixed
+     */
+    protected function renderChain(ViewInterface $view) {
+        // Children
+        $data = [];
+        $dataByObject = new \SplObjectStorage();
+
+        foreach ($view->getChildren() as $name => $child) {
+            if (isset($dataByObject[$child])) {
+                $data[$name] = $dataByObject[$child];
+
+                continue;
+            }
+
+            $data[$name] =
+            $dataByObject[$child] = $this->renderChain($child);
+        }
+
+        // Variables
+        $variables = $view->getVariables();
+
+        if ($names = array_intersect_key($data, $variables)) {
+            throw new KeyCollisionException(sprintf(static::EXCEPTION_NAME_COLLISION, implode("', '", array_keys($names))));
+        }
+
+        return $data + $variables;
+    }
+}
--- classes/Fixin/View/Engine/JsonEngine.php
+++ classes/Fixin/View/Engine/JsonEngine.php
@@ -7,11 +7,9 @@
 
 namespace Fixin\View\Engine;
 
-use Fixin\Base\Exception\KeyCollisionException;
-use Fixin\ResourceManager\Resource;
 use Fixin\View\ViewInterface;
 
-class JsonEngine extends Resource implements EngineInterface {
+class JsonEngine extends Engine {
 
     const EXCEPTION_NAME_COLLISION = "Child-variable name collision: '%s'";
 
@@ -20,37 +18,6 @@ class JsonEngine extends Resource implements EngineInterface {
      * @see \Fixin\View\Engine\EngineInterface::render()
      */
     public function render(ViewInterface $view) {
-        return $this->container->get('Base\Json\Json')->encode($this->renderArray($view));
-    }
-
-    /**
-     * @param ViewInterface $view
-     * @throws KeyCollisionException
-     * @return array
-     */
-    protected function renderArray(ViewInterface $view): array {
-        // Children
-        $data = [];
-        $dataByObject = new \SplObjectStorage();
-
-        foreach ($view->getChildren() as $name => $child) {
-            if (isset($dataByObject[$child])) {
-                $data[$name] = $dataByObject[$child];
-
-                continue;
-            }
-
-            $data[$name] =
-            $dataByObject[$child] = $this->renderArray($child);
-        }
-
-        // Variables
-        $variables = $view->getVariables();
-
-        if ($names = array_intersect_key($data, $variables)) {
-            throw new KeyCollisionException(sprintf(static::EXCEPTION_NAME_COLLISION, implode("', '", array_keys($names))));
-        }
-
-        return $data + $variables;
+        return $this->container->get('Base\Json\Json')->encode($this->renderChain($view));
     }
 }
