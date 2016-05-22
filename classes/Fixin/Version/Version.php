d7232e039b1d544f049d3baeccb863f7ff7fffc9
AttilaJenei <www@attilajenei.com>
2016-05-23 00:08:58 +0200

    PHP version

--- classes/Fixin/Version/Version.php
+++ classes/Fixin/Version/Version.php
@@ -10,6 +10,11 @@ namespace Fixin\Version;
 final class Version {
 
     /**
+     * @var string
+     */
+    const MINIMUM_PHP_VERSION = '7.0';
+
+    /**
      * Installed version
      */
     const VERSION = '0.1';
