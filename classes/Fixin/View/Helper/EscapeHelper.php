ba5430d48079814df0a519039cc3350039e192f6
AttilaJenei <www@attilajenei.com>
2016-05-23 18:31:10 +0200

    Abstract view helpers

---
+++ classes/Fixin/View/Helper/EscapeHelper.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Fixin Framework
+ *
+ * @copyright  Copyright (c) 2016 Attila Jenei
+ */
+
+namespace Fixin\View\Helper;
+
+use Fixin\ResourceManager\ResourceManagerInterface;
+
+abstract class EscapeHelper extends Helper {
+
+    /**
+     * @var EscaperInterface
+     */
+    protected $escaper;
+
+    /**
+     * @param ResourceManagerInterface $container
+     * @param array $options
+     * @param string $name
+     */
+    public function __construct(ResourceManagerInterface $container, array $options = null, string $name = null) {
+        parent::__construct($container, $options, $name);
+
+        $this->escaper = $container->get('Base\Escaper\Escaper');
+    }
+
+    /**
+     * Escape invoke
+     *
+     * @param mixed $value
+     * @return string
+     */
+    public function __invoke($value) {
+        if (is_array($value)) {
+            return array_map([$this, 'escape'], $value);
+        }
+
+        return $this->escape($value);
+    }
+
+    /**
+     * Escape value
+     *
+     * @param string $value
+     * @return string
+     */
+    abstract function escape(string $value): string;
+}
---
+++ classes/Fixin/View/Helper/Helper.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Fixin Framework
+ *
+ * @copyright  Copyright (c) 2016 Attila Jenei
+ */
+
+namespace Fixin\View\Helper;
+
+use Fixin\View\Engine\EngineInterface;
+use Fixin\ResourceManager\Resource;
+
+abstract class Helper extends Resource implements HelperInterface {
+
+    /**
+     * @var EngineInterface
+     */
+    protected $engine;
+
+    /**
+     * {@inheritDoc}
+     * @see \Fixin\View\Helper\HelperInterface::withEngine($engine)
+     */
+    public function withEngine(EngineInterface $engine): HelperInterface {
+        $clone = clone $this;
+        $clone->engine = $engine;
+
+        return $clone;
+    }
+}
