6edcc5499a28d5c02a11d9b38036305343889051
AttilaJenei <www@attilajenei.com>
2016-05-23 02:13:16 +0200

    PhpEngine render

--- classes/Fixin/View/Engine/PhpEngine.php
+++ classes/Fixin/View/Engine/PhpEngine.php
@@ -7,6 +7,41 @@
 
 namespace Fixin\View\Engine;
 
+use Fixin\View\ViewInterface;
+
 class PhpEngine extends Engine {
 
+    /**
+     * {@inheritDoc}
+     * @see \Fixin\View\Engine\Engine::renderChain()
+     */
+    protected function renderChain(ViewInterface $view) {
+        $__data = parent::renderChain($view);
+
+        // Template
+        $__template = $view->getResolvedTemplate();
+        if (is_null($__template)) {
+            return $__data;
+        }
+
+        // Extract data
+        unset($view);
+        unset($__data['this']);
+        extract($__data);
+        unset($__data);
+
+        // Include
+        try {
+            ob_start();
+            include $__template;
+            $content = ob_get_clean();
+        }
+        catch (\Throwable $t) {
+            ob_end_clean();
+
+            throw $t;
+        }
+
+        return $content;
+    }
 }
