27cf37570ea74e498a675c7eea8db93a89289a91
AttilaJenei <www@attilajenei.com>
2016-05-24 17:49:12 +0200

    Ground::performanceMaesure(), ::debugText()

--- classes/Fixin/Support/Ground.php
+++ classes/Fixin/Support/Ground.php
@@ -10,9 +10,16 @@ namespace Fixin\Support;
 class Ground extends DoNotCreate {
 
     /**
-     * @var bool
+     * Display debug text for environment
+     *
+     * @param string $html
+     * @return string
      */
-    protected static $isConsole;
+    public static function debugText(string $html) {
+        return static::isConsole()
+        ? htmlspecialchars_decode(strip_tags($html))
+        : '<div style="font-family: monospace; white-space: pre; color: #000; line-height: 1.05">' . $html . '</div>';
+    }
 
     /**
      * Check if running in CLI mode
@@ -20,6 +27,43 @@ class Ground extends DoNotCreate {
      * @return boolean
      */
     public static function isConsole() {
-        return static::$isConsole ?? (static::$isConsole = PHP_SAPI === 'cli');
+        static $isConsole = null;
+
+        return $isConsole ?? ($isConsole = PHP_SAPI === 'cli');
+    }
+
+    /**
+     * Measurement for performance tests
+     *
+     * Elapsed time, memory change, memory peak, memory system peak
+     */
+    public static function performanceMaesure() {
+        $memoryUsage = memory_get_usage();
+
+        // Previous values
+        static $lastTime = null;
+        static $lastMemoryUsage = null;
+
+        // Start
+        if (is_null($lastTime)) {
+            echo static::debugText("[Performance Measurement start]\n");
+
+            $lastTime = microtime(true);
+            $lastMemoryUsage = memory_get_usage();
+
+            return;
+        }
+
+        // Info
+        $info = "\nElapsed time:        " . number_format((microtime(true) - $lastTime) * 1000, 4) . " ms\n";
+        $info .= 'Memory change:       ' . number_format($memoryUsage - $lastMemoryUsage) . "\n";
+        $info .= 'Memory peak:         ' . number_format(memory_get_peak_usage()) . "\n";
+        $info .= 'Memory system peak:  ' . number_format(memory_get_peak_usage(true)) . "\n\n";
+
+        echo static::debugText($info);
+
+        // Store current
+        $lastTime = microtime(true);
+        $lastMemoryUsage = $memoryUsage;
     }
 }
--- classes/Fixin/Support/ToStringTrait.php
+++ classes/Fixin/Support/ToStringTrait.php
@@ -18,10 +18,7 @@ trait ToStringTrait {
      */
     public function __toString(): string {
         $items = method_exists($this, '__debugInfo') ? $this->__debugInfo() : (array) $this;
-        $description = get_class($this) . ' {' . ($items ? "\n" . VariableInspector::itemsInfo($items, '#444') : '') . '}';
 
-        return Ground::isConsole()
-            ? htmlspecialchars_decode(strip_tags($description))
-            : '<div style="font-family: monospace; white-space: pre; color: #000; line-height: 1.05">' . $description . '</div>';
+        return Ground::debugText(get_class($this) . ' {' . ($items ? "\n" . VariableInspector::itemsInfo($items, '#444') : '') . '}');
     }
 }
