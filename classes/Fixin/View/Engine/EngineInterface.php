8c1996c2a1f6ce5960c4f7734d1cb887a52b92b5
AttilaJenei <www@attilajenei.com>
2016-05-25 03:50:49 +0200

    getHelper()

--- classes/Fixin/View/Engine/EngineInterface.php
+++ classes/Fixin/View/Engine/EngineInterface.php
@@ -7,9 +7,18 @@
 
 namespace Fixin\View\Engine;
 
+use Fixin\View\Helper\HelperInterface;
 use Fixin\View\ViewInterface;
 
 interface EngineInterface {
+
+    /**
+     * Get helper
+     *
+     * @param string $name
+     * @return HelperInterface
+     */
+    public function getHelper(string $name): HelperInterface;
 
     /**
      * Render view
