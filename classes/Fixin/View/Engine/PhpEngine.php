839352b67a131f4e779f8090c37ffcd7c91d8479
AttilaJenei <www@attilajenei.com>
2016-05-26 12:23:42 +0200

    Remove of Stream

--- classes/Fixin/View/Engine/PhpEngine.php
+++ classes/Fixin/View/Engine/PhpEngine.php
@@ -14,17 +14,11 @@ use Fixin\View\ViewInterface;
 class PhpEngine extends Engine {
 
     const NO_TEMPLATE = 'No template';
-    const STREAM_MEMORY_SIZE = 256 * 1024;
 
     /**
      * @var AssistantInterface
      */
     protected $assistant;
-
-    /**
-     * @var Stream[]
-     */
-    protected $streamStack = [];
 
     /**
      * @param ResourceManagerInterface $container
@@ -35,18 +29,6 @@ class PhpEngine extends Engine {
         parent::__construct($container, $options, $name);
 
         $this->assistant = $this->container->clonePrototype('View\Engine\Assistant')->withEngine($this);
-    }
-
-    /**
-     * Writing to stream
-     *
-     * @param string $string
-     * @return string
-     */
-    protected function streamHandler(string $string) {
-        end($this->streamStack)->write($string);
-
-        return '';
     }
 
     /**
@@ -63,23 +45,17 @@ class PhpEngine extends Engine {
         // Data
         $data = $this->renderChildren($view) + $view->getVariables();
 
-        // Stream
-        $this->streamStack[] = new Stream('php://temp/maxmemory:' . static::STREAM_MEMORY_SIZE, 'wb+');
-
         // Include
         try {
-            ob_start([$this, 'streamHandler'], static::STREAM_MEMORY_SIZE);
+            ob_start();
             EncapsulatedInclude::include(clone $this->assistant, $filename, $data);
         }
         catch (\Throwable $t) {
             ob_end_clean();
-            array_pop($this->streamStack);
 
             throw $t;
         }
 
-        ob_end_clean();
-
-        return array_pop($this->streamStack);
+        return ob_get_clean();
     }
 }
