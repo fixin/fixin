d27a4e45ad2412eb52dce662bcd8566e9ace993e
AttilaJenei <www@attilajenei.com>
2016-05-30 00:11:08 +0200

    Application update

--- classes/Fixin/Application/Application.php
+++ classes/Fixin/Application/Application.php
@@ -7,28 +7,27 @@
 
 namespace Fixin\Application;
 
-use Fixin\Delivery\Cargo\CargoInterface;
-use Fixin\Delivery\Cargo\Cargo;
-use Fixin\View\View;
-
 class Application implements ApplicationInterface {
 
-    const DEFAULT_RESOURCE_MANAGER_CLASS = 'Fixin\ResourceManager\ResourceManager';
+    const DEFAULT_RESOURCE_MANAGER_CLASS = 'Fixin\Resource\ResourceManager';
     const DEFAULT_RESOURCE_MANAGER_CONFIG_CLASS = 'Fixin\Base\Config\Config';
 
     const INTERNAL_SERVER_ERROR_HEADER = 'HTTP/1.1 500 Internal Server Error';
     const INTERNAL_SERVER_ERROR_HTML = '<h1>500 Internal server error</h1>';
 
-    const KEY_APPLICATION_DISPATCHER = 'applicationDispatcher';
+    const KEY_APPLICATION = 'application';
     const KEY_CARGO = 'cargo';
     const KEY_CLASS = 'class';
     const KEY_CONFIG = 'config';
     const KEY_CONFIG_CLASS = 'configClass';
-    const KEY_ERROR_DISPATCHER = 'errorDispatcher';
+    const KEY_ERROR_ROUTE = 'errorRoute';
     const KEY_RESOURCE_MANAGER = 'resourceManager';
+    const KEY_ROUTE = 'route';
+
+    protected $config;
 
     /**
-     * @var \Fixin\ResourceManager\ResourceManagerInterface
+     * @var \Fixin\Resource\ResourceManagerInterface
      */
     protected $container;
 
@@ -36,6 +35,10 @@ class Application implements ApplicationInterface {
      * @param array $config
      */
     public function __construct(array $config) {
+        // Config
+        $this->config = $config[static::KEY_APPLICATION] ?? [];
+        unset($config[static::KEY_APPLICATION]);
+
         // Resource Manager config
         $containerConfig = $config[static::KEY_RESOURCE_MANAGER];
         unset($config[static::KEY_RESOURCE_MANAGER]);
@@ -61,13 +64,15 @@ class Application implements ApplicationInterface {
      * @throws \Throwable
      */
     protected function errorRoute($cargo) {
+        $container = $this->container;
+
         try {
             if ($cargo instanceof \Throwable) {
-                $cargo = (new Cargo())->setContent($cargo);
+                $cargo = $container->clonePrototype('Delivery\Cargo\Cargo')->setContent($cargo);
             }
 
             // Error dispatch
-            $cargo = $this->container->clonePrototype(static::KEY_ERROR_DISPATCHER)->dispatch($cargo);
+            $cargo = $container->get($this->config[static::KEY_ERROR_ROUTE])->dispatch($cargo);
             $cargo->unpack();
         }
         catch (\Throwable $t) {
@@ -83,10 +88,13 @@ class Application implements ApplicationInterface {
     public function run() {
         $container = $this->container;
 
+        $sessionManager = $container->get('Base\Session\SessionManager');
+        $sessionManager->getSession('abcde');
+
         try {
             // Normal dispatch
-            $cargo = $container->clonePrototype(static::KEY_CARGO);
-            $cargo = $container->clonePrototype(static::KEY_APPLICATION_DISPATCHER)->dispatch($cargo);
+            $cargo = $container->clonePrototype($this->config[static::KEY_CARGO]);
+            $cargo = $container->get($this->config[static::KEY_ROUTE])->dispatch($cargo);
             $cargo->unpack();
         }
         catch (\Throwable $t) {
