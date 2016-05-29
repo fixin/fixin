e5e3b29c74ef3c5ac3a7c130bf71e0ca9aeddae4
AttilaJenei <www@attilajenei.com>
2016-05-29 15:43:11 +0200

    Prototype, PrototypeInterface

---
+++ classes/Fixin/Resource/Prototype.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Fixin Framework
+ *
+ * @copyright  Copyright (c) 2016 Attila Jenei
+ */
+
+namespace Fixin\Resource;
+
+abstract class Prototype extends Resource implements PrototypeInterface {
+
+}
