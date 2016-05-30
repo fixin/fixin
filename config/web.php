5f07249278c0e0e213d7e3cdb41a04f6f34d881b
AttilaJenei <www@attilajenei.com>
2016-05-30 14:39:44 +0200

    Default FileSystem

--- config/web.php
+++ config/web.php
@@ -24,6 +24,7 @@ return [
                     ]
                 ]
             ],
+            'Base\FileSystem\Default' => 'Base\FileSystem\Local',
             'Base\Session\SessionRepository' => [
                 'class' => 'Base\Model\Repository',
                 'options' => [
