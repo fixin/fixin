06efce672e1cf16d91b72933735ca50f4d70afdb
AttilaJenei <www@attilajenei.com>
2016-05-30 16:46:47 +0200

    Remove of config

--- classes/Fixin/Application/Application.php
+++ classes/Fixin/Application/Application.php
@@ -7,27 +7,24 @@
 
 namespace Fixin\Application;
 
-use Fixin\Base\Storage\Directory\Index;
-
 class Application implements ApplicationInterface {
 
     const CONFIG_APPLICATION = 'application';
     const CONFIG_RESOURCE_MANAGER = 'resourceManager';
 
     const DEFAULT_RESOURCE_MANAGER_CLASS = 'Fixin\Resource\ResourceManager';
-    const DEFAULT_RESOURCE_MANAGER_CONFIG_CLASS = 'Fixin\Base\Config\Config';
 
     const INTERNAL_SERVER_ERROR_HEADER = 'HTTP/1.1 500 Internal Server Error';
     const INTERNAL_SERVER_ERROR_HTML = '<h1>500 Internal server error</h1>';
 
     const OPTION_CARGO = 'cargo';
     const OPTION_CLASS = 'class';
-    const OPTION_CONFIG_CLASS = 'configClass';
     const OPTION_ERROR_ROUTE = 'errorRoute';
     const OPTION_ROUTE = 'route';
 
-    const RESOURCE_CONFIG = 'config';
-
+    /**
+     * @var array
+     */
     protected $config;
 
     /**
@@ -41,21 +38,13 @@ class Application implements ApplicationInterface {
     public function __construct(array $config) {
         // Config
         $this->config = $config[static::CONFIG_APPLICATION] ?? [];
-        unset($config[static::CONFIG_APPLICATION]);
 
         // Resource Manager config
         $containerConfig = $config[static::CONFIG_RESOURCE_MANAGER];
-        unset($config[static::CONFIG_RESOURCE_MANAGER]);
 
-        // Classes
+        // Class
         $containerClass = $containerConfig[static::OPTION_CLASS] ?? static::DEFAULT_RESOURCE_MANAGER_CLASS;
         unset($containerConfig[static::OPTION_CLASS]);
-
-        $configClass = $containerConfig[static::OPTION_CONFIG_CLASS] ?? static::DEFAULT_RESOURCE_MANAGER_CONFIG_CLASS;
-        unset($containerConfig[static::OPTION_CONFIG_CLASS]);
-
-        // Config
-        $containerConfig['resources'][static::RESOURCE_CONFIG] = new $configClass($config);
 
         // Resoure Manager init
         $this->container = new $containerClass($containerConfig);
@@ -91,6 +80,11 @@ class Application implements ApplicationInterface {
      */
     public function run() {
         $container = $this->container;
+
+        $view = $container->clonePrototype('View\View');
+        $view->setTemplate('layout/test');
+
+        echo $view->render();
 
         try {
             // Normal dispatch
