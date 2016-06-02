f6c602a56ec63a9337ac3ba7803aab41e2682888
AttilaJenei <www@attilajenei.com>
2016-06-02 18:47:21 +0200

    ControllerInterface

---
+++ classes/Fixin/Controller/ControllerInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Fixin Framework
+ *
+ * @copyright  Copyright (c) 2016 Attila Jenei
+ */
+
+namespace Fixin\Controller;
+
+use Fixin\Delivery\Cargo\CargoHandlerInterface;
+
+interface ControllerInterface extends CargoHandlerInterface {
+
+}
