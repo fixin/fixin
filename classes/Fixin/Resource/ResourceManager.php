3a304ab1b49a8b78982f16f800cee1e1c0917ee7
AttilaJenei <www@attilajenei.com>
2016-06-07 02:23:40 +0200

    canProduceByAbstractFactory()

--- classes/Fixin/Resource/ResourceManager.php
+++ classes/Fixin/Resource/ResourceManager.php
@@ -10,6 +10,22 @@ namespace Fixin\Resource;
 class ResourceManager extends ResourceManagerBase {
 
     /**
+     * Determine resource creation via an abstract factory is possible
+     *
+     * @param string $name
+     * @return bool
+     */
+    protected function canProduceByAbstractFactory(string $name): bool {
+        foreach ($this->abstractFactories as $abstractFactory) {
+            if ($abstractFactory->canProduce($name)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * {@inheritDoc}
      * @see \Fixin\Resource\ResourceManagerInterface::clonePrototype()
      */
@@ -30,18 +46,6 @@ class ResourceManager extends ResourceManagerBase {
      * @see \Fixin\Support\ContainerInterface::has($name)
      */
     public function has(string $name): bool {
-        // Made or defined
-        if (isset($this->definitions[$name]) || class_exists($name)) {
-            return true;
-        }
-
-        // Abstract factories
-        foreach ($this->abstractFactories as $abstractFactory) {
-            if ($abstractFactory->canProduce($name)) {
-                return true;
-            }
-        }
-
-        return false;
+        return isset($this->definitions[$name]) || class_exists($name) || $this->canProduceByAbstractFactory($name);
     }
 }
