8a4ae190997ad3904515ea7542f5748acedaa357
AttilaJenei <www@attilajenei.com>
2016-06-15 13:42:11 +0200

    EntitySet skeleton

---
+++ classes/Fixin/Model/Entity/EntitySetInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Fixin Framework
+ *
+ * @copyright  Copyright (c) 2016 Attila Jenei
+ */
+
+namespace Fixin\Model\Entity;
+
+use Fixin\Resource\PrototypeInterface;
+
+interface EntitySetInterface extends PrototypeInterface {
+
+}
