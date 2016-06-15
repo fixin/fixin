e19cbcba2854882dba0d2870436f091b0ffb98f0
AttilaJenei <www@attilajenei.com>
2016-06-16 00:13:31 +0200

    Basic methods

--- classes/Fixin/Model/Entity/EntitySetInterface.php
+++ classes/Fixin/Model/Entity/EntitySetInterface.php
@@ -7,8 +7,68 @@
 
 namespace Fixin\Model\Entity;
 
+use Countable;
 use Fixin\Resource\PrototypeInterface;
+use Iterator;
 
-interface EntitySetInterface extends PrototypeInterface {
+interface EntitySetInterface extends PrototypeInterface, Iterator, Countable {
 
+    /**
+     * Append another entity set
+     *
+     * @param self $entitySet
+     * @return self
+     */
+    public function append(self $entitySet): self;
+
+    /**
+     * Get column values
+     *
+     * @param string $name
+     * @return array
+     */
+    public function getColumn(string $name): array;
+
+    /**
+     * Get entity IDs of all items
+     *
+     * @return array
+     */
+    public function getEntityIds(): array;
+
+    /**
+     * Get repository
+     *
+     * @return RepositoryInterface
+     */
+    public function getRepository(): RepositoryInterface;
+
+    /**
+     * Prefetch all entities
+     *
+     * @return self
+     */
+    public function prefetchAll(): self;
+
+    /**
+     * Refault all fetched entities to IDs
+     *
+     * @return self
+     */
+    public function refault(): self;
+
+    /**
+     * Set count of entities for prefetch
+     *
+     * @param int $prefetchSize
+     * @return self
+     */
+    public function setPrefetchSize(int $prefetchSize): self;
+
+    /**
+     * Shuffle entities
+     *
+     * @return self
+     */
+    public function shuffle(): self;
 }
