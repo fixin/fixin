921572065bffd6bccf82dbf4a42ad4a553d39f52
AttilaJenei <www@attilajenei.com>
2016-06-16 00:13:14 +0200

    deleteEntity(), fetchEntity()

--- classes/Fixin/Model/Entity/EntityIdInterface.php
+++ classes/Fixin/Model/Entity/EntityIdInterface.php
@@ -16,6 +16,20 @@ interface EntityIdInterface extends PrototypeInterface {
     const OPTION_REPOSITORY = 'repository';
 
     /**
+     * Delete entity of ID
+     *
+     * @return EntityIdInterface
+     */
+    public function deleteEntity(): self;
+
+    /**
+     * Fetch entity of ID
+     *
+     * @return EntityInterface|null
+     */
+    public function fetchEntity();
+
+    /**
      * Get repository of ID
      *
      * @return RepositoryInterface
