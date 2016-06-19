acc477a2d6583673fec699273001214a16d5cc06
AttilaJenei <www@attilajenei.com>
2016-06-19 20:07:50 +0200

    all(), selectEntities(), selectRawData()

--- classes/Fixin/Model/Repository/RepositoryInterface.php
+++ classes/Fixin/Model/Repository/RepositoryInterface.php
@@ -9,8 +9,9 @@ namespace Fixin\Model\Repository;
 
 use Fixin\Model\Entity\EntityIdInterface;
 use Fixin\Model\Entity\EntityInterface;
-use Fixin\Model\Entity\EntitySetInterface;
 use Fixin\Resource\ResourceInterface;
+use Fixin\Model\Storage\StorageResultInterface;
+use Fixin\Model\Entity\EntitySetInterface;
 
 interface RepositoryInterface extends ResourceInterface {
 
@@ -20,6 +21,13 @@ interface RepositoryInterface extends ResourceInterface {
     const OPTION_PRIMARY_KEY = 'primaryKey';
     const OPTION_REQUEST_PROTOTYPE = 'requestPrototype';
     const OPTION_STORAGE = 'storage';
+
+    /**
+     * All entities
+     *
+     * @return EntitySetInterface
+     */
+    public function all(): EntitySetInterface;
 
     /**
      * Create entity for the repository
@@ -37,20 +45,12 @@ interface RepositoryInterface extends ResourceInterface {
     public function createId(...$entityId): EntityIdInterface;
 
     /**
-     * Delete record(s)
+     * Delete
      *
      * @param RepositoryRequestInterface $request
      * @return int
      */
     public function delete(RepositoryRequestInterface $request): int;
-
-    /**
-     * Get entities
-     *
-     * @param RepositoryRequestInterface $request
-     * @return EntitySetInterface
-     */
-//     public function get(RepositoryRequestInterface $request): EntitySetInterface;
 
     /**
      * Get name of the repository
@@ -60,7 +60,7 @@ interface RepositoryInterface extends ResourceInterface {
     public function getName(): string;
 
     /**
-     * Insert record(s) by request into repository
+     * Insert into
      *
      * @param RepositoryInterface $repository
      * @param RepositoryRequestInterface $request
@@ -69,7 +69,23 @@ interface RepositoryInterface extends ResourceInterface {
     public function insertInto(RepositoryInterface $repository, RepositoryRequestInterface $request): int;
 
     /**
-     * Update record(s)
+     * Select entities
+     *
+     * @param RepositoryRequestInterface $request
+     * @return EntitySetInterface
+     */
+    public function selectEntities(RepositoryRequestInterface $request): EntitySetInterface;
+
+    /**
+     * Select raw data
+     *
+     * @param RepositoryRequestInterface $request
+     * @return StorageResultInterface
+     */
+    public function selectRawData(RepositoryRequestInterface $request): StorageResultInterface;
+
+    /**
+     * Update
      *
      * @param array $set
      * @param RepositoryRequestInterface $request
@@ -78,10 +94,11 @@ interface RepositoryInterface extends ResourceInterface {
     public function update(array $set, RepositoryRequestInterface $request): int;
 
     /**
-     * New request instance
+     * New request
      *
      * @param mixed $where
      * @return RepositoryRequestInterface
      */
     public function where($where): RepositoryRequestInterface;
+
 }
