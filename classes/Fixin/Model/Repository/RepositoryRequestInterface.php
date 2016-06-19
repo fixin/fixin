967cb30dce3fe693795eebcea155d4ae2adf0c81
AttilaJenei <www@attilajenei.com>
2016-06-20 00:49:36 +0200

    where(), limit()

--- classes/Fixin/Model/Repository/RepositoryRequestInterface.php
+++ classes/Fixin/Model/Repository/RepositoryRequestInterface.php
@@ -52,12 +52,28 @@ interface RepositoryRequestInterface extends PrototypeInterface {
     public function getRepository(): RepositoryInterface;
 
     /**
+     * Limit count
+     *
+     * @param int $limit
+     * @return self
+     */
+    public function limit(int $limit): self;
+
+    /**
      * Update
      *
      * @param array $set
      * @return int
      */
     public function update(array $set): int;
+
+    /**
+     * Where
+     *
+     * @param array|\Closure $where
+     * @return self
+     */
+    public function where($where): self;
 
     /*
      * avg
