42063761d4b9ee82b78ab581e982b5b3ebda6f8e
AttilaJenei <www@attilajenei.com>
2016-08-28 16:22:22 +0200

    Skeleton

--- classes/Fixin/Model/Storage/Pdo/MysqlGrammar.php
+++ classes/Fixin/Model/Storage/Pdo/MysqlGrammar.php
@@ -7,8 +7,49 @@
 
 namespace Fixin\Model\Storage\Pdo;
 
+use Fixin\Model\Repository\RepositoryInterface;
+use Fixin\Model\Request\RequestInterface;
 use Fixin\Resource\Resource;
 
 class MysqlGrammar extends Resource implements GrammarInterface {
 
+    /**
+     * {@inheritDoc}
+     * @see \Fixin\Model\Storage\Pdo\GrammarInterface::delete($request)
+     */
+    public function delete(RequestInterface $request): string {
+        // TODO
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Fixin\Model\Storage\Pdo\GrammarInterface::insert($repository, $set)
+     */
+    public function insert(RepositoryInterface $repository, array $set): string {
+        // TODO
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Fixin\Model\Storage\Pdo\GrammarInterface::insertInto($repository, $request)
+     */
+    public function insertInto(RepositoryInterface $repository, RequestInterface $request): string {
+        // TODO
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Fixin\Model\Storage\Pdo\GrammarInterface::select($request)
+     */
+    public function select(RequestInterface $request): string {
+        // TODO
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Fixin\Model\Storage\Pdo\GrammarInterface::update($set, $request)
+     */
+    public function update(array $set, RequestInterface $request): string {
+        // TODO
+    }
 }
