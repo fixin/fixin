cb0e0eafeba6d6b22654d2124020eda133591ef4
AttilaJenei <www@attilajenei.com>
2016-09-18 16:35:46 +0200

    refresh() and save() implementation

--- classes/Fixin/Model/Entity/Entity.php
+++ classes/Fixin/Model/Entity/Entity.php
@@ -17,16 +17,15 @@ abstract class Entity extends Prototype implements EntityInterface {
     use ToStringTrait;
 
     const
-        EXCEPTION_NOT_STORED_ENTITY = 'Not stored entity',
-        THIS_SETS_LAZY = [
-            self::OPTION_REPOSITORY => RepositoryInterface::class
-        ]
-    ;
+    EXCEPTION_NOT_STORED_ENTITY = 'Not stored entity',
+    THIS_SETS_LAZY = [
+        self::OPTION_REPOSITORY => RepositoryInterface::class
+    ];
 
     /**
-     * @var bool
+     * @var array
      */
-    protected $deleted = false;
+    protected $deletableRelatedEntities = array();
 
     /**
      * @var EntityIdInterface
@@ -39,19 +38,13 @@ abstract class Entity extends Prototype implements EntityInterface {
     protected $repository;
 
     /**
-     * @return array
-     */
-    abstract protected function collectSaveData(): array;
-
-    /**
      * {@inheritDoc}
      * @see \Fixin\Model\Entity\EntityInterface::delete()
      */
     public function delete(): EntityInterface {
         if ($this->isStored()) {
-            if ($this->deleted = $this->entityId->deleteEntity()) {
-                $this->entityId = null;
-            }
+            $this->entityId->deleteEntity();
+            $this->entityId = null;
 
             return $this;
         }
@@ -77,14 +70,6 @@ abstract class Entity extends Prototype implements EntityInterface {
 
     /**
      * {@inheritDoc}
-     * @see \Fixin\Model\Entity\EntityInterface::isDeleted()
-     */
-    public function isDeleted(): bool {
-        return $this->deleted;
-    }
-
-    /**
-     * {@inheritDoc}
      * @see \Fixin\Model\Entity\EntityInterface::isStored()
      */
     public function isStored(): bool {
@@ -93,22 +78,40 @@ abstract class Entity extends Prototype implements EntityInterface {
 
     /**
      * {@inheritDoc}
+     * @see \Fixin\Model\Entity\EntityInterface::refresh()
+     */
+    public function refresh() {
+        if ($this->isStored()) {
+            $this->getRepository()->refresh($this);
+
+            return $this;
+        }
+
+        throw new NotStoredEntityException(static::EXCEPTION_NOT_STORED_ENTITY);
+    }
+
+    /**
+     * {@inheritDoc}
      * @see \Fixin\Model\Entity\EntityInterface::save()
      */
     public function save(): EntityInterface {
-        // TODO
+        $this->entityId = $this->getRepository()->save($this);
+        $this->saveRelated();
 
-        if ($this->entityId) {
-            $request = $this->getRepository()->createRequest();
-            $request->getWhere()->items($this->entityId);
-            $request->update($this->collectSaveData());
+        return $this;
+    }
 
-            return $this->refresh();
+    /**
+     * Save related entities
+     *
+     * @return \Fixin\Model\Entity\Entity
+     */
+    protected function saveRelated() {
+        foreach ($this->deletableRelatedEntities as $entity) {
+            $entity->delete();
         }
 
-        $this->entityId = $this->getRepository()->insert($this->collectSaveData());
-        $this->deleted = false; // <- move to refresh()
-        $this->refresh(); // TODO
+        $this->deletableRelatedEntities = [];
 
         return $this;
     }
