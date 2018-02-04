28ee1f3bf945e9fe81eeab82106fe0d4dcfc4465
Attila Jenei <work@attilajenei.com>
2018-02-04 01:23:37 +0000

    Dictionary

---
+++ classes/Base/Dictionary/Exception/DirectoryExceptionInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * /Fixin Framework
+ *
+ * Copyright (c) Attila Jenei
+ *
+ * http://www.fixinphp.com
+ */
+
+namespace Fixin\Base\Dictionary\Exception;
+
+use Fixin\Exception\FixinExceptionInterface;
+
+interface DirectoryExceptionInterface extends FixinExceptionInterface
+{
+}
