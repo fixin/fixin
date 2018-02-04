f48c2e7075a2d15efd130bde7932da414c844c63
Attila Jenei <work@attilajenei.com>
2018-02-04 22:08:06 +0000

    PHPDoc

--- classes/Model/Storage/StorageInterface.php
+++ classes/Model/Storage/StorageInterface.php
@@ -16,14 +16,53 @@ use Fixin\Resource\ResourceInterface;
 
 interface StorageInterface extends ResourceInterface
 {
+    /**
+     * Delete
+     */
     public function delete(RequestInterface $request): int;
+
+    /**
+     * Get last insert value
+     */
     public function getLastInsertValue(): int;
+
+    /**
+     * Insert
+     */
     public function insert(RepositoryInterface $repository, array $set): int;
+
+    /**
+     * Insert into
+     */
     public function insertInto(RepositoryInterface $repository, RequestInterface $request): int;
+
+    /**
+     * Insert multiple
+     */
     public function insertMultiple(RepositoryInterface $repository, array $rows): int;
+
+    /**
+     * Select
+     */
     public function select(RequestInterface $request): StorageResultInterface;
+
+    /**
+     * Select column
+     */
     public function selectColumn(RequestInterface $request): StorageResultInterface;
+
+    /**
+     * Select exists value
+     */
     public function selectExistsValue(RequestInterface $request): bool;
+
+    /**
+     * Convert value to DateTime
+     */
     public function toDateTime($value): ?DateTimeImmutable;
+
+    /**
+     * Update
+     */
     public function update(array $set, RequestInterface $request): int;
 }
